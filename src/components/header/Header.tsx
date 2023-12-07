a1872b1a3356f8371eddd2b380e20794972d724b
naralicecosta <naralicecosta04@gmail.com>
2023-12-07 19:27:59 -0300

    login e logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,18 @@
 import {BiLogOut} from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
+import { auth } from '../../services/firebaseConnection'
+import { signOut } from 'firebase/auth'
+
 export function Header(){
+    async function handleLogout(){
+       await signOut(auth);
+
+    }
     return(
-        <header>
-            <nav>
-                <div>
+        <header className='w-full max-w-2xl mt-4 px-1'>
+            <nav className='W-FULL bg-white h-12 flex items-center justify-between rounded-md px-3'>
+                <div className='flex gap-4 font-medium'>
                     <Link to="/">
                         Home
                     </Link>
@@ -17,7 +24,7 @@ export function Header(){
                     </Link>
                 </div>
 
-                <button>
+                <button onClick={handleLogout}>
                     <BiLogOut size={28} color="#db2629"
 />
                 </button>
